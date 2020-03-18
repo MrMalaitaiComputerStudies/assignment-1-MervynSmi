3067d302b1a15f2fc7e5d0f62549a4e108c05958
Mervyn Smith <mervyn_smi@student.asdah.school.nz>
2020-03-19 12:26:12 +1300

    created a GUI

--- text.py
+++ text.py
@@ -1,5 +1,15 @@
+# Created by Mervyn Smith
 import tkinter as tk
 
 root = tk.Tk()
+
+f_name = tk.Label(root, text="First Name:")
+f_name.grid(row = 0, column = 0)
+
+e1 = tk.Entry(root)
+e1.grid(row = 1, column = 0)
+
+b1 = tk.Button(root, text="Submit")
+b1.grid(row = 2, column = 0, columnspan = 3)
 
 root.mainloop()
